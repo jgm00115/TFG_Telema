b8f8aa2285812a52ecc7ecd363fccebb1d09eb37
jgm00115 <jgm00115@outlook.es>
2023-03-23 08:37:36 +0100

    implementa Web Audio

--- client/src/components/Stream.jsx
+++ client/src/components/Stream.jsx
@@ -4,18 +4,25 @@ import TrackSelector from './TrackSelector';
 
 import { useRef, useEffect, useState } from 'react'
 
-export default function Stream({mediaURL}) {
+export default function Stream({ mediaURL }) {
 
     // Variables sin estado (su valor persiste entre re-renderizados)
     const numChannels = useRef(0);
     const audioRef = useRef(null);
     const player = useRef(dashjs.MediaPlayer().create());
 
+    const audioChainInitialized = useRef(false);
+    const audioCtx = useRef(null);
+    const srcNode = useRef(null);
+    const splitterNode = useRef(null);
+    const gainNodes = useRef(Array(0));
+
+
     // Variables con estado (cada vez que cambien, los componentes se re-renderizan)
     const [numTracks, setNumTracks] = useState(0);
     const [track, setTrack] = useState(0);
     const [gains, setGains] = useState(Array(0).fill(0));
-    
+
     // Inicializa el reproductor dash y lo vincula al elemento de audio
     // Se ejecuta una única vez tras el primer renderizado
     useEffect(() => {
@@ -36,16 +43,39 @@ export default function Stream({mediaURL}) {
 
     }, []);
 
-    // Acciones que se ejecutan cada vez que se renderiza el componente
+    // Se ejecuta cuando ganancias se actualicen
     useEffect(() => {
-        console.log(`Ganancias = ${gains}`);
-        console.log(`Track activo = ${track}`);
-        console.log(`numTracks = ${numTracks}`);
-        console.log(`numChannels = ${numChannels.current}`);
 
-        // Actualiza el número de
+        console.log(`Ganancias = ${gains}`);
+
+        // SI LA CADENA DE AUDIO ESTÁ INICIALIZADA
+        if (audioChainInitialized.current) {
+
+            // Actualiza nodos de ganancia
+            for (let i = 0; i < gains.length; i++) {
+                gainNodes.current[i].gain.value = gains[i];
+            }
+            
+        }
+
+    },[gains]);
+
+    // Se ejecuta cuando el track seleccionado se actualice
+    useEffect(() => {
+
+        console.log(`Track activo = ${track}`);
         
-    });
+        if (audioChainInitialized.current) {
+
+            // Actualiza track activo
+
+            const tracks = player.current.getTracksFor('audio');
+
+            player.current.setCurrentTrack(tracks[track]);
+            
+        }
+
+    },[track]);
 
     // Maneja el evento onCanPlay
     const canPlay = () => {
@@ -56,15 +86,60 @@ export default function Stream({mediaURL}) {
 
         console.log(`Número de tracks de audio = ${numTracks}`);
 
-        // Actualiza el número de tracks de audio disponibles
+        // Actualiza el número de canales de audio disponibles
         numChannels.current = tracks.map(
-            (track)=> track.audioChannelConfiguration);
+            (track) => track.audioChannelConfiguration);
 
         console.log(`Número de canales por track = ${numChannels.current}`);
 
         // Actualiza el número de ganancias necesarias
         setGains(Array(Math.max(...numChannels.current)).fill(0))
-        
+    }
+
+    const onPlay = () => {
+        // SI CADENA DE AUDIO ESTÁ INICIALIZADA, VUELVE
+        if (audioChainInitialized.current)
+            return;
+
+        // INICIALIZA CADENA DE AUDIO 
+        // Crea un audio context
+        audioCtx.current = new AudioContext();
+
+        // Crea un nodo fuente
+        srcNode.current = audioCtx.current.createMediaElementSource(audioRef.current);
+
+        srcNode.current.channelCount = Math.max(...numChannels.current);
+        srcNode.current.channelInterpretation = 'discrete';
+        // Crea un splitter con tantas salidas como máximo número de canales hay
+        splitterNode.current = audioCtx.current.createChannelSplitter(Math.max(...numChannels.current));
+
+        // Conecta el nodo fuente al splitter
+        srcNode.current.connect(splitterNode.current);
+
+        // Crea nodos de ganancia
+        for (let i = 0; i < gains.length; i++) {
+
+            gainNodes.current.push(audioCtx.current.createGain());
+
+            // Conecta la salida del splitter al nodo de ganancia correspondiente
+            splitterNode.current.connect(gainNodes.current[i], i);
+
+            // Conecta el nodo de ganancia a la salida
+            gainNodes.current[i].connect(audioCtx.current.destination);
+        }
+
+        // Asigna ganancias por defecto
+        for (let i = 0; i < gains.length; i++) {
+            gainNodes.current[i].gain.value = gains[i];
+        }
+
+        // Selecciona el track 0 por defecto
+        const tracks = player.current.getTracksFor('audio');
+
+        player.current.setCurrentTrack(tracks[0]);
+
+        // Cadena de audio inicializada
+        audioChainInitialized.current = true;
     }
 
     return (
@@ -72,6 +147,7 @@ export default function Stream({mediaURL}) {
             <audio
                 ref={audioRef}
                 onCanPlay={canPlay}
+                onPlay={onPlay}
                 controls>
             </audio>
 
