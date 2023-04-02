7b22d12d74b9fb683c49e5f2e40fbf45ff514c99
jgm00115 <jgm00115@outlook.es>
2023-04-02 22:47:26 +0200

    eliminacion de evento can play por ser redundante

--- client/src/components/Stream.jsx
+++ client/src/components/Stream.jsx
@@ -2,29 +2,27 @@ import dashjs from 'dashjs'
 import Mixer from './Mixer'
 import TrackSelector from './TrackSelector';
 
-import {AudioChain} from './AudioChain';
+import { AudioChain } from './AudioChain';
 
 import { useRef, useEffect, useState } from 'react'
 
 export default function Stream({ mediaURL }) {
 
     // Variables sin estado (su valor persiste entre re-renderizados)
-    const numChannels = useRef(0);
-    const audioRef = useRef(null);
-    const player = useRef(dashjs.MediaPlayer().create());
-    
+    const numTracks = useRef(0);                            // número de tracks de audio total
+    const numChannels = useRef(0);                          // canales de audio por track (array)
+    const audioRef = useRef(null);                          // html audio media element 
+    const player = useRef(dashjs.MediaPlayer().create());   // dash player
+
     const audioChain = useRef(null);
 
     // Variables con estado (cada vez que cambien, los componentes se re-renderizan)
-    const [numTracks, setNumTracks] = useState(0);
-    const [track, setTrack] = useState(0);
-    const [gains, setGains] = useState(Array(0).fill(0));
+    const [track, setTrack] = useState(0);                  // track activo
+    const [gains, setGains] = useState(Array(0).fill(0));   // array con ganancias de cada fader
 
-    // Inicializa el reproductor dash y lo vincula al elemento de audio
     // Se ejecuta una única vez tras el primer renderizado
     useEffect(() => {
 
-        // Inicializa el reproductor dash y lo vincula al elemento de audio
         player.current.updateSettings({
             streaming: {
                 cacheInitSegments: true,
@@ -37,14 +35,10 @@ export default function Stream({ mediaURL }) {
             }
         })
 
+        // Inicializa el reproductor dash y lo vincula al elemento de audio (autoplay true)
         player.current.initialize(audioRef.current, mediaURL, true);
 
     }, []);
-
-    // Dispara el evento cuando el reproductor se para porque le falta data
-    player.current.on(dashjs.MediaPlayer.events.PLAYBACK_WAITING, (e)=> {
-        console.log(`Evento ${JSON.stringify(e)}`)
-    })
 
     // Se ejecuta cuando ganancias se actualicen
     useEffect(() => {
@@ -56,16 +50,16 @@ export default function Stream({ mediaURL }) {
 
             // Actualiza nodos de ganancia
             audioChain.current.setFadersGain(gains);
-            
+
         }
 
-    },[gains]);
+    }, [gains]);
 
     // Se ejecuta cuando el track seleccionado se actualice
     useEffect(() => {
 
         console.log(`Track activo = ${track}`);
-        
+
         if (audioChain.current != null) {
 
             // Actualiza track activo
@@ -73,47 +67,45 @@ export default function Stream({ mediaURL }) {
             const tracks = player.current.getTracksFor('audio');
 
             player.current.setCurrentTrack(tracks[track]);
-            
+
         }
 
-    },[track]);
+    }, [track]);
 
-    // Maneja el evento onCanPlay
-    const canPlay = () => {
+    const onPlay = () => {
+
+        const defaultGain = 0.5;
+
+        // SI CADENA DE AUDIO ESTÁ INICIALIZADA, VUELVE
+        if (audioChain.current != null)
+            return;
 
         // Actualiza el valor del número de tracks disponibles
         const tracks = player.current.getTracksFor('audio');
-        setNumTracks(tracks.length);
+        numTracks.current = tracks.length;
 
-        console.log(`Número de tracks de audio = ${numTracks}`);
+        console.log(`Número de tracks de audio = ${numTracks.current}`);
 
         // Actualiza el número de canales de audio disponibles
         numChannels.current = tracks.map(
             (track) => track.audioChannelConfiguration);
 
+        const maxNumChannels = Math.max(...numChannels.current);
+
         console.log(`Número de canales por track = ${numChannels.current}`);
+        console.log(`Número de canales máximo = ${maxNumChannels}`);
 
-        // Actualiza el número de ganancias necesarias
-        setGains(Array(Math.max(...numChannels.current)).fill(0.5))
-    }
+        // INICIALIZA CADENA DE AUDIO
+        audioChain.current = new AudioChain(audioRef.current,
+            numTracks.current, maxNumChannels, maxNumChannels, defaultGain);
 
-    const onPlay = () => {
-        // SI CADENA DE AUDIO ESTÁ INICIALIZADA, VUELVE
-        if (audioChain.current != null)
-            return;
+        console.log(`Nueva cadena de audio creada`);
 
-        // INICIALIZA CADENA DE AUDIO 
-        audioChain.current = new AudioChain(audioRef.current,numTracks,numChannels.current);
-        
-        // Asigna ganancias por defecto
-        audioChain.current.setFadersGain(gains);
+        // Actualiza el valor de las ganancias
+        setGains(audioChain.current.getFadersGain());
 
         // Selecciona el track 0 por defecto
-        const tracks = player.current.getTracksFor('audio');
-
         player.current.setCurrentTrack(tracks[0]);
-
-        console.log(`Cadena de audio inicializada`);
 
     }
 
@@ -121,13 +113,12 @@ export default function Stream({ mediaURL }) {
         <div>
             <audio
                 ref={audioRef}
-                onCanPlay={canPlay}
                 onPlay={onPlay}
                 controls>
             </audio>
 
             <TrackSelector
-                numTracks={numTracks}
+                numTracks={numTracks.current}
                 track={track}
                 setTrack={setTrack}
             />
