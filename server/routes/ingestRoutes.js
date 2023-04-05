70cb5d85285c4bbd84755fae804cccb3a5c419a1
jgm00115 <jgm00115@outlook.es>
2023-04-05 13:23:51 +0200

    elimina express.raw middleware

--- server/routes/ingestRoutes.js
+++ server/routes/ingestRoutes.js
@@ -6,10 +6,10 @@ const express = require('express');
 const router = express.Router();
 
 // controladores
-const stream_controller = require('../controllers/streamController');
+const ingest_controller = require('../controllers/ingestController');
 
-router.get('/:stream_key', stream_controller.get_ingest);
+router.get('/:stream_key', ingest_controller.get_ingest);
 
-router.post('/:stream_key/:filename', express.raw(), stream_controller.stream_ingest);
+router.post('/:stream_key/:filename', ingest_controller.stream_ingest);
 
 module.exports = router;
