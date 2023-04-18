06eb94cdc6abc08e557823bc47b99b1729ca3c8e
jgm00115 <jgm00115@outlook.es>
2023-04-18 15:56:19 +0200

    añadidos modelos de base de datos, controladores y rutas para HRTFs

--- server/main.js
+++ server/main.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-
+const connect = require('./utils/database');
 const app = express();
 
 app.use(cors());
 
 // rutas
 const ingestRoutes = require('./routes/ingestRoutes');
+const hrtfRoutes = require('./routes/hrtfRoutes');
 
 app.use('/ingest',ingestRoutes);
+app.use('/hrtf',hrtfRoutes);
 
 // directorio raíz
 rootDir = path.resolve(__dirname,'..');
 
 // directorio con recursos estáticos
-app.use(express.static('public'));
+app.use(express.static(path.join(rootDir,'public')));
 
 const port = 8080;
 
@@ -45,6 +47,19 @@ app.get('/media/:media/:stream/:segment', (req,res) => {
 
 })
 
-app.listen(port, () => {
-    console.log(`Servidor escuchando en puerto ${port}`);
-})
+async function startServer() {
+    try {
+        // Lee la url de la base de datos desde las variables de entorno
+        const mongoUrl = process.env.MONGO_URL;
+        await connect(mongoUrl);
+        console.log(`Conectado a la base de datos ${mongoUrl}`);
+
+        app.listen(port, () => {
+            console.log(`Servidor escuchando en puerto ${port}`);
+        });
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+startServer();
