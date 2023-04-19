2c672417b2b43ea7e27518d087b1260781503c2d
jgm00115 <jgm00115@outlook.es>
2023-04-19 19:51:06 +0200

    endpoints para coordenadas disponibles

--- server/routes/hrtfRoutes.js
+++ server/routes/hrtfRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const hrtfController = require('../controllers/hrtfController');
 
 router.get('/', hrtfController.getHRTF);
-router.post('/', express.json(), hrtfController.postHRTF);
+router.post('/', express.json(), hrtfController.postHRTF);  //usa middleware para parsear payload a json
+
+//Coordenadas disponibles
+router.get('/availableCoords', hrtfController.getAvailableCoords);
 
 module.exports = router;
