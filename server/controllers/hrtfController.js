42c1a8d47bda3bace5335e5b8d07364e5836c3bb
jgm00115 <jgm00115@outlook.es>
2023-04-19 19:51:06 +0200

    endpoints para coordenadas disponibles

--- server/controllers/hrtfController.js
+++ server/controllers/hrtfController.js
@@ -1,8 +1,36 @@
 const HRTF = require('../models/hrtf');
 
+// Devuelve todas las HRTF
 exports.getHRTF = (req,res) => {
-    res.send('No implementado');
+    HRTF.find({})
+    .then(documents => {
+        res.json(documents);
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+    })
 }
+
+// Devuelve las coordenadas azimuth y elevation disponibles
+exports.getAvailableCoords = (req,res) => {
+    Promise.all([
+        HRTF.distinct('azimuth'),
+        HRTF.distinct('elevation')
+    ])
+    .then(([azValues, elValues]) => {
+        const coords = {
+            azimuths: azValues,
+            elevations: elValues
+        };
+        res.json(coords);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.sendStatus(500);
+    });
+}
+
 
 exports.postHRTF = (req, res) => {
     const hrtf = new HRTF({
