dd41ab33925c6b7f99b886bd226a20ea36016c84
jgm00115 <jgm00115@outlook.es>
2023-04-19 14:13:08 +0200

    usa un indice compuesto para asegurar hrtf unicas en una posicion del espacio

--- server/models/hrtf.js
+++ server/models/hrtf.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const HRTFSchema = new Schema({
-    azimuth: {type: Number, required: true, unique: true},
-    elevation: {type: Number, required: true, unique: true},
+    azimuth: {type: Number, required: true},
+    elevation: {type: Number, required: true},
     left: {type: [Number], required: true},
     right: {type: [Number], required:true},
 });
+// Añade un índice compuesto por los campos azimuth y elevation
+HRTFSchema.index({azimuth: 1, elevation: 1 }, {unique: true});
 
 module.exports = mongoose.model('HRTF',HRTFSchema);
