5cb172287299eed1eeb66a4e700f98a6a98eb56f
jgm00115 <jgm00115@outlook.es>
2023-04-24 09:48:30 +0200

    endpoint para recuperar todos los streamings

--- server/routes/streamRoutes.js
+++ server/routes/streamRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const streamController = require('../controllers/streamController');
 
+router.get('/', streamController.getStreams);
 router.post('/', express.json(), streamController.postStream);  //usa middleware para parsear payload a json
 router.get('/:id/hrtfs', streamController.getStreamHRTFS);
 
