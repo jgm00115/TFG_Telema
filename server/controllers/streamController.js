d0457e3387fa39f2144ad0bf24ce106d3f3c7f17
jgm00115 <jgm00115@outlook.es>
2023-04-26 22:23:53 +0200

    endpoint para rotación de posiciones

--- server/controllers/streamController.js
+++ server/controllers/streamController.js
@@ -1,4 +1,5 @@
 const Stream = require('../models/stream');
+const HRTF = require('../models/hrtf');
 
 // Devuelve todos los streams
 exports.getStreams = (req,res) => {
@@ -47,4 +48,73 @@ exports.getStreamHRTFS = (req,res) => {
         console.log(err);
         res.sendStatus(500);
     })
+}
+
+// Devuelve un set de HRTFS añadiendo rotación a la espacializacion
+exports.rotate = async (req, res) => {
+    const streamID = req.params.id;
+    const rotation = parseInt(req.params.rotation);
+    try {
+        const positions = await findPositions(streamID);
+        console.log(`Posiciones base: ${JSON.stringify(positions)}`);
+        if (positions){
+            // Añade la rotación a cada posición
+            for (const position of positions){
+                position.azimuth +=rotation;
+            }
+            console.log(`Posiciones tras rotación: ${JSON.stringify(positions)}`);
+            // Encuentra las posiciones más cercanas disponibles
+            closestAvailablePositions = await closestAvailableAzimuths(positions);
+            console.log(`Posiciones disponibles más cercanas:
+            ${JSON.stringify(closestPositions)}`);
+            // Query de hrtfs de esas posiciones
+            const hrtf = await Promise.all(
+                closestAvailablePositions.map((position)=> {
+                    return HRTF.find({azimuth:position.azimuth,elevation:position.elevation});
+                }));
+            res.status(200).json(hrtf);
+        }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+    
+
+}
+
+// Encuentra las posiciones de cada instrumento
+findPositions = async (streamID) => {
+    const stream = await Stream.findById(streamID);
+    if (!stream)
+        return null;
+    const instruments = stream.instruments;
+    const positions = [];
+    for (const instrument of instruments){
+        positions.push({
+            azimuth: instrument.azimuth,
+            elevation: instrument.elevation
+        });
+    }
+    return positions;
+}
+
+/* Para cada posicion selecciona la posicion más cercana
+*/
+closestAvailableAzimuths = async (positions) => {
+    closestPositions= [];
+    for (const position of positions){
+        azimuth = position.azimuth;
+        elevation = position.elevation;
+        // Azimuths disponibles para esa elevación
+        const availableAzimuths = await HRTF.distinct(
+            'azimuth',{elevation: elevation});
+        // Distancia angular con los azimuths disponibles
+        const dist = availableAzimuths.map(
+            (availableAzimuth) => Math.abs(availableAzimuth - azimuth));
+        // Posición con mínima distancia
+        closestPositions.push({
+            azimuth: availableAzimuths[dist.indexOf(Math.min(...dist))],
+            elevation: elevation});
+    }
+    return closestPositions;
 }
--- server/routes/streamRoutes.js
+++ server/routes/streamRoutes.js
@@ -6,5 +6,6 @@ const streamController = require('../controllers/streamController');
 router.get('/', streamController.getStreams);
 router.post('/', express.json(), streamController.postStream);  //usa middleware para parsear payload a json
 router.get('/:id/hrtfs', streamController.getStreamHRTFS);
+router.get('/:id/hrtfs/:rotation', streamController.rotate);
 
 module.exports = router;
